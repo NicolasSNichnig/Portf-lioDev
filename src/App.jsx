e276195ec8fa9983c888e92c130f219c050e05d4
NÍCOLAS RAFAEL SANTOS NICHNIG <nicolas_nichnig@estudante.sesisenai.org.br>
2023-10-24 13:42:25 -0300

    <Sua mensagem>

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/header"
+
+export default function App() {
+  return (
+    <div className="w-screen h-screen">
+      <div>
+        <Header />
+      </div>
+    </div>
+  )
+}
+
---
+++ src/components/header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <div className="w-screen">
+            <p className="text-center"> Aqui vai o header</p>
+        </div>
+    )
+}
